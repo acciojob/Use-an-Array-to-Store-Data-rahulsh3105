b94e86e2f3530ad95863f109de83780d58f5340f
Rahul Sharma <rahulsh3105@gmail.com>
2023-03-10 19:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-let yourArray = ['hello', 42, true, null, 'world'];
-console.log(yourArray.length);
+let yourArray = [1, 2, 'three', true, null];
+console.log(yourArray); // output: [1, 2, 'three', true]
+console.log(yourArray.length); // output: 4
+console.log(yourArray[2]); // output: 'three'
