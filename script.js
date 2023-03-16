308986693fe3c73c93969f455c12e62f3350a89c
Rahul Sharma <rahulsh3105@gmail.com>
2023-03-16 13:30:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-let yourArray;
-// assign your array the correct value in the next line
-// your code here!
-// code end
-
-yourArray = ['one', 2, 'three', true, 5, false];
-
-module.exports = yourArray;
+let yourArray = ["hello", 42, true, null, "world"];
